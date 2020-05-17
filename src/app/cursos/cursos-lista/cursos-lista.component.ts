4d64dec60b59b9a8a84a8e6044c0545c03d987f0
Leandro de Almeida <leandroleekrap@gmail.com>
2020-05-16 21:42:52 -0300

    capturando erros

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CursosService } from '../cursos.service';
 import { Curso } from '../curso';
-import { Observable } from 'rxjs';
+import { Observable, empty, Subject } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -11,13 +12,40 @@ import { Observable } from 'rxjs';
 })
 export class CursosListaComponent implements OnInit {
 
-  cursos$: Observable<Curso[]>
+  cursos$: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private service: CursosService) { }
 
   ngOnInit() {
+    this.Atualizar();
     
-    this.cursos$ = this.service.list();
   }
 
+  Atualizar(){
+    this.cursos$ = this.service.list()
+    .pipe(
+     // map(),
+     // tap(),
+     // switchMap(),
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return empty();
+      })
+    );
+      this.service.list()
+        .pipe(
+          catchError(error => empty())
+        )
+      .subscribe(
+          dados => {
+            console.log(dados);
+          }
+          //error => console.error(error),
+          //() => console.log('Obserservable completo!')
+      );
+  }
 }
+
+//10:51
